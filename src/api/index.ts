c71822aecd7cf0f7e72884c2be5702fda904c299
Egor Muzychkin <emuzych@gmail.com>
2023-12-20 19:32:20 +0300

    implement Crud class
    for reusable code

--- src/api/index.ts
+++ src/api/index.ts
@@ -14,6 +14,7 @@ import {
   getFirestore,
   Query,
   query,
+  QueryConstraint,
   serverTimestamp,
   setDoc,
   where,
@@ -44,53 +45,68 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// type TUId = {
-//   user_id: string
-// }
+type TUId = {
+  user_id: string;
+};
 
-// class Crud<CreateType, NewType extends TUId & CreateType, NormalType extends TId> {
-//   private collectionName: string
-//   private collectionRef: CollectionReference
-//   private firestore: Firestore
+class Crud<
+  CreateType,
+  NewType extends TUId & CreateType,
+  NormalType extends TId
+> {
+  private collectionName: string;
+  private collectionRef: CollectionReference;
+  private firestore: Firestore;
 
-//   constructor(firestore: Firestore, collectionName: string) {
-//     this.collectionName = collectionName
-//     this.firestore = firestore
-//     this.collectionRef = collection(firestore, collectionName)
-//   }
+  constructor(firestore: Firestore, collectionName: string) {
+    this.collectionName = collectionName;
+    this.firestore = firestore;
+    this.collectionRef = collection(firestore, collectionName);
+  }
 
-//   public read = async (docId: string) => {
-//     const docRaw = await getDoc(doc(this.firestore, this.collectionName, docId))
-//     const docData = normalizeData<NormalType>(docRaw)
-//     return docData
-//   }
-//   public readAll = async (query?: Query) => {
-//     const collectionRaw = await getDocs(query ?? collection(this.firestore, this.collectionName))
-//     const collectionNormalized = normalizeDataArray<NormalType>(collectionRaw.docs)
-//     return collectionNormalized
-//   }
+  public read = async (docId: string) => {
+    const docRaw = await getDoc(
+      doc(this.firestore, this.collectionName, docId)
+    );
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const docData = normalizeData<NormalType>(docRaw);
+    return docData;
+  };
+  public readAll = async (...queryConstrains: QueryConstraint[]) => {
+    const collectionRaw = await getDocs(
+      query(collection(this.firestore, this.collectionName), ...queryConstrains)
+    );
+    const collectionNormalized = normalizeDataArray<NormalType>(
+      collectionRaw.docs
+    );
+    return collectionNormalized;
+  };
 
-//   public create = async (createBody: CreateType) => {
-//     const auth = getAuth()
-//     // TODO: Продумать как добавить поле created_at, чтобы можно было передавать
-//     const newThing: NewType = {
-//       user_id: auth.currentUser?.uid ?? "",
-//       ...createBody
-//     }
-//     const newDocRef = await addDoc(this.collectionRef, newThing)
-//     const createdDoc = await this.read(newDocRef.id)
-//     return createdDoc
-//   }
+  public create = async (createBody: CreateType) => {
+    const auth = getAuth();
+    // TODO: Продумать как добавить поле created_at, чтобы можно было передавать
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const newThing: NewType = {
+      user_id: auth.currentUser?.uid ?? "",
+      ...createBody,
+    };
+    const newDocRef = await addDoc(this.collectionRef, newThing);
+    const createdDoc = await this.read(newDocRef.id);
+    return createdDoc;
+  };
 
-//   public update = async (docId: string, updateBody: Partial<NewType>) => {
-//     const docRef = doc(this.firestore, this.collectionName, docId)
-//     await setDoc(docRef, updateBody, { merge: true });
-//     const updated = this.read(docRef.id)
-//     return updated
-//   }
+  public update = async (docId: string, updateBody: Partial<NewType>) => {
+    const docRef = doc(this.firestore, this.collectionName, docId);
+    await setDoc(docRef, updateBody, { merge: true });
+    const updated = this.read(docRef.id);
+    return updated;
+  };
 
-//   // TODO: delete method
-// }
+  // TODO: delete method
+  public delete = async (docId: string) => {};
+}
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
@@ -98,17 +114,17 @@ export const firebaseApp = initializeApp(firebaseConfig);
 export class API {
   private firebaseApp: FirebaseApp;
   private fireStore: Firestore;
-  private accountsRef: CollectionReference;
-  private transactionsRef: CollectionReference;
-  private deptsRef: CollectionReference;
+  private accounts: Crud<TCreateAccount, TNewAccount, TAccount>;
+  private transactions: Crud<TCreateTransaction, TNewTransaction, TTransaction>;
+  // private depts: Crud;
 
   constructor(firebaseApp: FirebaseApp) {
     this.firebaseApp = firebaseApp;
     this.fireStore = getFirestore(firebaseApp);
 
-    this.accountsRef = collection(this.fireStore, "accounts");
-    this.transactionsRef = collection(this.fireStore, "transactions");
-    this.deptsRef = collection(this.fireStore, "depts");
+    this.accounts = new Crud(this.fireStore, "accounts");
+    this.transactions = new Crud(this.fireStore, "transactions");
+    // this.depts = collection(this.fireStore, "depts");
   }
 
   /**
@@ -130,22 +146,19 @@ export class API {
 
   private getAccountsQuery = () => {
     const currentUser = getAuth().currentUser;
-    const q = query(this.accountsRef, where("user_id", "==", currentUser?.uid));
+    const q = where("user_id", "==", currentUser?.uid ?? "");
     return q;
   };
 
   public getAccounts = async (): Promise<ReadonlyArray<TAccount>> => {
     const q = this.getAccountsQuery();
-    const accountsRaw = await getDocs(q);
-    const accountsNormalized = normalizeDataArray<TAccount>(accountsRaw.docs);
+    const accounts = await this.accounts.readAll(q);
     // TODO: сохранить список счетов в хранилище
-    return accountsNormalized;
+    return accounts;
   };
 
   public getAccount = async (accountId: string) => {
-    const accountRaw = await getDoc(doc(this.fireStore, "accounts", accountId));
-    // TODO: сохранить счет в хранилище
-    const account = normalizeData<TAccount>(accountRaw);
+    const account = await this.accounts.read(accountId);
     return account;
   };
 
@@ -156,9 +169,7 @@ export class API {
     accountId: string,
     toUpdate: Partial<TNewAccount>
   ): Promise<TAccount> => {
-    const accountRef = doc(this.fireStore, "accounts", accountId);
-    await setDoc(accountRef, toUpdate, { merge: true });
-    const updatedAccount = (await getDoc(accountRef)).data() as TAccount;
+    const updatedAccount = await this.accounts.update(accountId, toUpdate);
     return updatedAccount;
   };
 
@@ -167,18 +178,12 @@ export class API {
    * @param createAccount account body to create new account
    */
   public createAccount = async (createAccount: TCreateAccount) => {
-    const auth = getAuth();
+    // TODO: придумать как настроить created_at через функ create в Crud
     const newAccountPrepeared: TNewAccount = {
-      user_id: auth.currentUser?.uid ?? "",
       created_at: serverTimestamp(),
       ...createAccount,
     };
-    const newAccountResponse = await addDoc(
-      this.accountsRef,
-      newAccountPrepeared
-    );
-    // console.log((await getDoc(newAccountResponse)).data());
-    // TODO: this.getAccounts()
+    const newAccountResponse = await this.accounts.create(newAccountPrepeared);
     return newAccountResponse;
   };
 
@@ -193,54 +198,29 @@ export class API {
   //! ------------------------- Transactions -------------------------
 
   private getTransactionsQuery = (accountId: string) => {
-    const q = query(this.transactionsRef, where("account_id", "==", accountId));
+    const q = where("account_id", "==", accountId);
     return q;
   };
 
   public getTransactionsForAccount = async (accountId: string) => {
-    // console.group("Inside getTransactionsForAccount()"); 
-    // console.log("accountId : ", accountId);
     const q = this.getTransactionsQuery(accountId);
-    console.log(
-      "before getDocs() in getTransactionsForAccount :"
-    );
-
-    const transactionsRaw = await getDocs(q);
-    console.log("after getDocs() in getTransactionsForAccount :", transactionsRaw);
-    
-    const transactionsNormalized = normalizeDataArray<TTransaction>(
-      transactionsRaw.docs
-    );
-    // console.log("transactionsNormalized : ", transactionsNormalized);
-
-    // console.groupEnd();
-    return transactionsNormalized;
+    const transactions = await this.transactions.readAll(q);
+    return transactions;
   };
 
   public createTransaction = async (transactionBody: TCreateTransaction) => {
-    const auth = getAuth();
     const newTransactionPrepeared: TNewTransaction = {
       created_at: serverTimestamp(),
-      user_id: auth.currentUser?.uid ?? "",
       ...transactionBody,
     };
-    console.log(
-      "addDoc with newTransactionPrepeared :",
-      newTransactionPrepeared
-    );
-    
-    const transactionRef = await addDoc(
-      this.transactionsRef,
-      newTransactionPrepeared
-    );
-    const transactionResponse = await getDoc(transactionRef);
-    const transaction = normalizeData<TTransaction>(transactionResponse);
+    const transaction = await this.transactions.create(newTransactionPrepeared);
     return transaction;
   };
 
   public createTransactionAndUpdateAccount = async (
     transactionBody: TCreateTransaction
   ) => {
+    // TODO: переписать все на batch или transaction, чтобы выполнять все записи за один запрос
     // TODO: добавить проверку на отрицательное число (нельзя потратить больше, чем есть на счету)
     const account = await this.getAccount(transactionBody.account_id);
     const newAccountValue =
@@ -248,15 +228,10 @@ export class API {
         ? account.value + transactionBody.value
         : account.value - transactionBody.value;
 
-    console.log("before create transaction");
     const transaction = await this.createTransaction(transactionBody);
-    console.log("after create transaction");
-    console.log("before update account");
     await this.updateAccount(transactionBody.account_id, {
       value: newAccountValue,
     });
-    console.log("after update account");
-
     return transaction;
   };
   //* ------------------------- -------------------------
@@ -265,50 +240,6 @@ export class API {
   public createDept = async () => {};
   //* ------------------------- -------------------------
 
-  public accounts = {
-    crud: Accounts,
-  };
-
-  public transactions = {
-    crud: Transactions,
-    /**
-     * Создает новую транзакцию и оновляет аккаунт
-     *
-     * 1. получить целевой аккаунт
-     * 2. проверить, есть ли в аккаунте копилка
-     *    - действия для копилки
-     * 3. посчитать новое значение аккаунта
-     *    - если < 0 вернуть ошибку
-     * 4. создаем новую транзакцию
-     * 5. обновляем значение аккаунта
-     */
-    createTransactionAndUpdateAccount: async (
-      newTransaction: NewTransaction
-    ) => {
-      // 1. find target account
-      const account = await this.accounts.crud.read(newTransaction.accountId);
-
-      // check if is expense or income
-      const isIncome = newTransaction.transactionType === "income";
-
-      const floatAccountValue = parseFloat(account.value),
-        floatIncomingValue = parseFloat(newTransaction.value ?? "");
-
-      // 3. новое значение аккаунта
-      const newAccountValue = isIncome
-        ? floatAccountValue + floatIncomingValue
-        : floatAccountValue - floatIncomingValue;
-
-      // 4. Создать новую транзакцию
-      this.transactions.crud.create(newTransaction);
-
-      // 5. Обновить значение аккаунта
-      this.accounts.crud.update(newTransaction.accountId, {
-        value: newAccountValue.toString(),
-      });
-      return;
-    },
-  };
   public depts = {
     crud: Depts,
     pay: async (dept: Dept, value: number, account: Account) => {
